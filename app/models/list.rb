aa798a03eaba26566ddb1bd10f50cbacab2304ca
Pierre Grandjean <pierrebigjean@gmail.com>
2021-08-13 15:26:35 +0200

    added background to show

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,7 +2,8 @@ class List < ApplicationRecord
   has_many :bookmarks, :dependent => :destroy
   has_many :movies, through: :bookmarks
   has_many :reviews
-
+  has_one_attached :photo
+  
   validates :name, presence: true
   validates :name, uniqueness: true
 end
